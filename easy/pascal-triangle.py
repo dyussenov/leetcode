0a13bd56f61888dbca0759e6c6dfcd5e89fb48af
dyussenov <dyusenov.aset.arm@gmail.com>
2023-06-11 18:17:58 +0600

    add pascal triangle

---
+++ easy/pascal-triangle.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def generate(self, numRows):
+
+        res = []
+        for i in range(numRows):
+            res.append([])
+            for j in range(i + 1):
+                if j == 0 or j == i:
+                    res[i].append(1)
+                else:
+                    res[i].append(res[i - 1][j - 1] + res[i - 1][j])
+        return res
