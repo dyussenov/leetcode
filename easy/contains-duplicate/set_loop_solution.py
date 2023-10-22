a606c9cc2cb4f0f3174b61c14a1050a0fa25e05c
dyssenov <dyusenov.aset.arm@gmail.com>
2023-10-22 14:15:21 +0600

    contains-duplicate

---
+++ easy/contains-duplicate/set_loop_solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    """
+    time complexity: O(n)
+    memory: O(n)
+    """
+
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        nums_set = set()
+        
+        for num in nums:
+            if num in nums_set:
+                return True
+            nums_set.add(num)
+            
+        return False
